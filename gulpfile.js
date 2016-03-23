4e20103a0bdeffca212c9989b4e7876086a7b6fa
Ben Pinali <ben@benpinali.com>
2016-03-22 19:40:42 -0500

    Including initial html and css, js carousel and images

--- gulpfile.js
+++ gulpfile.js
@@ -17,25 +17,22 @@ gulp.task('jshint', function () {
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
 // Compile Sass task
 gulp.task('sass', function () {
     return gulp.src('site/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('site/css'));
 });
-// Watch task
-gulp.task('watch', function () {
-    gulp.watch('site/js/*.js', ['jshint']);
-    gulp.watch('site/scss/*.scss', ['sass']);
-});
-// Default task
-gulp.task('default', ['jshint', 'sass', 'watch']);
+
 // Minify index
 gulp.task('html', function () {
     return gulp.src('site/index.html')
         .pipe(minifyHTML())
         .pipe(gulp.dest('build/'));
 });
+
+// JavaScript build task, removes whitespace and concatenates all files
 gulp.task('scripts', function () {
     return browserify('site/js/main.js')
         .bundle()
@@ -44,15 +41,29 @@ gulp.task('scripts', function () {
         .pipe(uglify())
         .pipe(gulp.dest('build/js'));
 });
+
 // Styles build task, concatenates all the files
 gulp.task('styles', function () {
     return gulp.src('site/css/*.css')
         .pipe(concat('styles.css'))
         .pipe(gulp.dest('build/css'));
 });
+
+// Image optimization task
 gulp.task('images', function () {
     return gulp.src('site/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('build/img'));
 });
-gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
+
+// Watch task
+gulp.task('watch', function () {
+    gulp.watch('site/js/*.js', ['jshint']);
+    gulp.watch('site/scss/*.scss', ['sass']);
+});
+
+// Default task
+gulp.task('default', ['jshint', 'sass', 'watch']);
+
+// Build task
+gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images'])
